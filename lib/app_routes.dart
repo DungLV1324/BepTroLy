9aba0b64580d92f5073f146fd6c30b4e7d37cffb
DESKTOP-HCP90OM\computer <duycuibap026@gmail.com>
2025-12-13 14:51:19 +0700

    UI: meal_plan

--- lib/app_routes.dart
+++ lib/app_routes.dart
@@ -1,8 +1,7 @@
+import 'features/ke_hoach/views/meal_planner_screen.dart';
+import 'package:beptroly/shared/layout/main_scaffold.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:beptroly/shared/layout/main_scaffold.dart';
-
-// --- IMPORT CÁC MÀN HÌNH ---
 import 'features/auth/views/login_screen.dart';
 import 'features/auth/views/register_screen.dart';
 import 'features/home/views/home_screen.dart';
@@ -10,30 +9,23 @@ import 'features/goi_y_mon_an/views/recipe_feed_screen.dart';
 import 'features/goi_y_mon_an/views/recipe_detail_screen.dart';
 import 'features/goi_y_mon_an/models/recipe_model.dart';
 import 'features/kho_nguyen_lieu/views/pantry_screen.dart';
-// Import các màn hình bạn đã có
-import 'features/ke_hoach/views/meal_planner_screen.dart';
 import 'features/ke_hoach/views/shopping_list_screen.dart';
 
 final _rootNavigatorKey = GlobalKey<NavigatorState>();
-final _shellNavigatorHomeKey = GlobalKey<NavigatorState>(
-  debugLabel: 'shellHome',
-);
-final _shellNavigatorPantryKey = GlobalKey<NavigatorState>(
-  debugLabel: 'shellPantry',
-);
-final _shellNavigatorPlannerKey = GlobalKey<NavigatorState>(
-  debugLabel: 'shellPlanner',
-);
-final _shellNavigatorShoppingKey = GlobalKey<NavigatorState>(
-  debugLabel: 'shellShopping',
-);
+final _shellNavigatorHomeKey = GlobalKey<NavigatorState>(debugLabel: 'shellHome');
+final _shellNavigatorPantryKey = GlobalKey<NavigatorState>(debugLabel: 'shellPantry');
+final _shellNavigatorPlannerKey = GlobalKey<NavigatorState>(debugLabel: 'shellPlanner');
+final _shellNavigatorShoppingKey = GlobalKey<NavigatorState>(debugLabel: 'shellShopping');
 
 final appRouter = GoRouter(
   navigatorKey: _rootNavigatorKey,
   initialLocation: '/login',
   routes: [
-    // --- 1. MÀN HÌNH KHÔNG CÓ BOTTOM BAR (Full Screen) ---
-    GoRoute(path: '/login', builder: (context, state) => const LoginScreen()),
+    GoRoute(
+      path: '/login',
+      builder: (context, state) => const LoginScreen(),
+    ),
+
     GoRoute(
       path: '/register',
       builder: (context, state) => const RegisterScreen(),
@@ -61,7 +53,6 @@ final appRouter = GoRouter(
         return MainScaffold(navigationShell: navigationShell);
       },
       branches: [
-        // Tab 1: Home
         StatefulShellBranch(
           navigatorKey: _shellNavigatorHomeKey,
           routes: [
@@ -72,7 +63,6 @@ final appRouter = GoRouter(
           ],
         ),
 
-        // Tab 2: Tủ lạnh
         StatefulShellBranch(
           navigatorKey: _shellNavigatorPantryKey,
           routes: [
@@ -83,26 +73,21 @@ final appRouter = GoRouter(
           ],
         ),
 
-        // Tab 3: Lên lịch (Thay Placeholder bằng màn hình thật)
         StatefulShellBranch(
           navigatorKey: _shellNavigatorPlannerKey,
           routes: [
             GoRoute(
               path: '/planner',
-              // Nếu chưa có file thật thì tạm dùng Scaffold, nếu có rồi thì đổi thành:
-              // builder: (context, state) => const MealPlannerScreen(),
               builder: (context, state) => const MealPlannerScreen(),
             ),
           ],
         ),
 
-        // Tab 4: Mua sắm (Thay Placeholder bằng màn hình thật)
         StatefulShellBranch(
           navigatorKey: _shellNavigatorShoppingKey,
           routes: [
             GoRoute(
               path: '/shopping',
-              // builder: (context, state) => const ShoppingListScreen(),
               builder: (context, state) => const ShoppingListScreen(),
             ),
           ],
